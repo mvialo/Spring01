bbb7af00922228ebc26cdb55018380888bfcc313
mvialo <manuella.vialo@outlook.fr>
2024-06-27 15:35:22 +0200

    Transfert du projet dans le dépôt distant

---
+++ src/test/java/com/queteun/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queteun.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
